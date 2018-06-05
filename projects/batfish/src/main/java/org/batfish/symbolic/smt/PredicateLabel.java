e4a52f2d0613dbde438e227fb120dbafd9a700db
Xiaolin Sun <xsun@colgate.edu>
2018-06-05 15:55:37 -0400

    find unfound predicate in faultloc

--- projects/batfish/src/main/java/org/batfish/symbolic/smt/PredicateLabel.java
+++ projects/batfish/src/main/java/org/batfish/symbolic/smt/PredicateLabel.java
@@ -43,27 +43,29 @@ public class PredicateLabel{
   private String intface_String;
   
   public PredicateLabel(labels type) {
-    this.type=type;
-    this.device=null;
-    this.intface=null;
+    this(type, null);
   }
   
   public PredicateLabel(labels type, String device) {
-    this.type=type;
-    this.device=device;
-    this.intface=null;
+    this(type,device,"null");
   }
   
   public PredicateLabel(labels type, String device, Interface face) {
     this.type=type;
     this.device=device;
     this.intface=face;
+    this.intface_String=face.toString();
   }
   
   public PredicateLabel(labels type, String device, String face) {
     this.type=type;
     this.device=device;
+    if (device!=null && device.equals("null"))
+      this.device=null;
     this.intface_String=face;
+    if (face!=null && face.equals("null"))
+      this.intface_String=null;
+    this.intface=null;
   }
   
   public String toString() {
@@ -85,6 +87,49 @@ public class PredicateLabel{
   }
   
   public labels gettype() {
-    return this.type;
+    return type;
+  }
+  
+  public String getdevice() {
+    if (device=="null")
+      return null;
+    return device;
+  }
+  
+  public Interface getintface() {
+    if (this.intface!=null)
+      return this.intface;
+    else
+      return null;
+  }
+  
+  public String getStrintface() {
+    if (this.intface_String=="null")
+      return null;
+    else 
+      return this.intface_String;
+  }
+  
+  @Override
+  public boolean equals (Object o1) {
+    if (o1 instanceof PredicateLabel) {
+    boolean type_b=false, device_b=false, intface_b =false;
+    if (((PredicateLabel) o1).gettype().equals(type))
+      type_b = true;
+    if (((((PredicateLabel) o1).getdevice()==null)&&(device==null))
+        ||(((PredicateLabel) o1).getdevice()!=null && ((PredicateLabel) o1).getdevice().equals(device)))
+        device_b=true;
+    
+    if ((((PredicateLabel) o1).getStrintface()==null&&this.intface_String==null)
+        ||(((PredicateLabel) o1).getStrintface()!=null&&((PredicateLabel) o1).getStrintface().equals(intface_String)))
+      intface_b=true;
+        
+    return (type_b && device_b && intface_b);
+//    if ((o1.gettype()==type) && (o1.getdevice()==device) && (this.intface_String==o1.getStrintface()))
+//      return true;
+//   
+//    return false;
+    }
+    return false;
   }
 }
