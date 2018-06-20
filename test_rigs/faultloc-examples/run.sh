7374d93f0b5f6fa907fcdae2b016b15d31b95c59
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2018-06-20 15:46:04 -0500

    Updated run.sh script to use nice container/testrig names

--- test_rigs/faultloc-examples/run.sh
+++ test_rigs/faultloc-examples/run.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-BASEDIR=test_rigs/faultloc-examples
+# Determine path to scripts
+if [[ $(uname) == "Darwin" ]]; then
+    SCRIPTPATH=`greadlink -f $0`
+else
+    SCRIPTPATH=`readlink -f $0`
+fi
+BASEDIR="`dirname $SCRIPTPATH`"
+source $BASEDIR/../../tools/batfish_functions.sh
+#BASEDIR=test_rigs/faultloc-examples
 OPTIONS=$BASEDIR/options
 
 \ls $BASEDIR | while read NETWORK; do
     if [ -d $BASEDIR/$NETWORK ]; then
+        echo -e "init-container -setname $NETWORK" | allinone -runmode interactive || exit 1
         POLICIES=$BASEDIR/$NETWORK/original/policies/reach.cmd
         ORIG_CONFIGS=$BASEDIR/$NETWORK/original/configs
         \ls $BASEDIR/$NETWORK | while read SCENARIO; do
@@ -19,7 +28,7 @@ OPTIONS=$BASEDIR/options
 
             echo -e "CHECK POLICIES"
             echo -e "====================================================="
-            cat $OPTIONS $POLICIES | allinone -runmode interactive -testrigdir $TESTRIG_DIR || exit 1
+            echo -e "set-container $NETWORK\ninit-testrig $TESTRIG_DIR $SCENARIO\n`cat $OPTIONS $POLICIES`" | allinone -runmode interactive || exit 1
             echo -e "\n====================================================="
         done
     fi
