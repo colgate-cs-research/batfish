63cbc84ac9d72c1c593c4a2644e1af7665f278f0
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2018-07-03 12:50:34 -0400

    Fixed policies for redistribute-triangle

--- test_rigs/faultloc-examples/redistribute-triangle/original/policies/reach.cmd
+++ test_rigs/faultloc-examples/redistribute-triangle/original/policies/reach.cmd
@@ -1,6 +1,6 @@
 get smt-reachability ingressNodeRegex="A", finalNodeRegex="C", srcIps=['101.0.0.0/8'], dstIps=['103.0.0.0/8'], failures=1
-get smt-reachability ingressNodeRegex="C", finalNodeRegex="A", srcIps=['103.0.0.0/8'], dstIps=['101.0.0.0/8'], failures=1
-get smt-reachability ingressNodeRegex="A", finalNodeRegex="B", srcIps=['101.0.0.0/8'], dstIps=['102.0.0.0/8'], failures=1
-get smt-reachability ingressNodeRegex="B", finalNodeRegex="A", srcIps=['102.0.0.0/8'], dstIps=['101.0.0.0/8'], failures=1
-get smt-reachability ingressNodeRegex="C", finalNodeRegex="B", srcIps=['103.0.0.0/8'], dstIps=['102.0.0.0/8'], failures=1
-get smt-reachability ingressNodeRegex="B", finalNodeRegex="C", srcIps=['102.0.0.0/8'], dstIps=['103.0.0.0/8'], failures=1
+get smt-reachability ingressNodeRegex="A", finalNodeRegex="C", srcIps=['1.0.0.0/16'], dstIps=['103.0.0.0/8'], failures=1
+get smt-reachability ingressNodeRegex="A", finalNodeRegex="C", srcIps=['3.0.0.0/16'], dstIps=['103.0.0.0/8'], failures=1
+get smt-reachability ingressNodeRegex="B", finalNodeRegex="C", srcIps=['2.0.0.0/16'], dstIps=['103.0.0.0/8']
+get smt-reachability ingressNodeRegex="B", finalNodeRegex="A", srcIps=['1.0.0.0/16'], dstIps=['101.0.0.0/8']
+get smt-reachability ingressNodeRegex="B", finalNodeRegex="A", srcIps=['2.0.0.0/16'], dstIps=['101.0.0.0/8']
