deeb0109ec974db079ca121cc32671e909b6f6a6
Xiaolin Sun <xsun@colgate.edu>
2018-07-03 15:43:56 -0400

    update png size in R

--- test_rigs/faultloc-examples/generategraph.R
+++ test_rigs/faultloc-examples/generategraph.R
@@ -1,13 +1,13 @@
-mydata = read.csv("/Users/student/Desktop/master.csv",header=TRUE,sep=",")
+mydata = read.csv("master.csv",header=TRUE,sep=",")
 noop=list()
-noop<-subset(mydata,experiment=="nooption")
-compute<-subset(mydata,experiment=="compute")
-minimize<-subset(mydata,experiment=="minimize")
-cs<-subset(mydata,experiment=="compute+slice")
-csm<-subset(mydata,experiment=="compute+slice+minimize")
-cm<-subset(mydata,experiment=="minimize+compute")
-sm<-subset(mydata,experiment=="slice+minimize")
-slice<-subset(mydata,experiment=="slice")
+noop<-subset(mydata,experiment=="noop")
+compute<-subset(mydata,experiment=="c")
+minimize<-subset(mydata,experiment=="m")
+cs<-subset(mydata,experiment=="cs")
+csm<-subset(mydata,experiment=="csm")
+cm<-subset(mydata,experiment=="cm")
+sm<-subset(mydata,experiment=="sm")
+slice<-subset(mydata,experiment=="s")
 examples=list()
 examples[1]=noop[1]
 examples[2]=compute[1]
@@ -17,7 +17,7 @@ examples[5]=slice[1]
 examples[6]=cs[1]
 examples[7]=csm[1]
 examples[8]=sm[1]
-png("/Users/student/Desktop/result/examples.png")
+png("examples.png",width = 800)
 boxplot (examples,data=examples, main="numbers of examples", xlab="Number of examples", ylab="Differnt experiment", names=c("noop","compute","minimize","c+m","slice","c+s","c+s+m","s+m"))
 dev.off()
 foundpred=list()
@@ -29,7 +29,7 @@ foundpred[5]=slice[2]
 foundpred[6]=cs[2]
 foundpred[7]=csm[2]
 foundpred[8]=sm[2]
-png("/Users/student/Desktop/result/foundpred.png")
+png("foundpred.png",width = 800)
 boxplot (foundpred,data=foundpred, main="numbers of foundpred", xlab="Number of foundpred", ylab="Differnt experiment", names=c("noop","compute","minimize","c+m","slice","c+s","c+s+m","s+m"))
 dev.off()
 unfoundpred=list()
@@ -41,7 +41,7 @@ unfoundpred[5]=slice[3]
 unfoundpred[6]=cs[3]
 unfoundpred[7]=csm[3]
 unfoundpred[8]=sm[3]
-png("/Users/student/Desktop/result/unfoundpred.png")
+png("unfoundpred.png",width = 800)
 boxplot (foundpred,data=foundpred, main="numbers of unfoundpred", xlab="Number of unfoundpred", ylab="Differnt experiment", names=c("noop","compute","minimize","c+m","slice","c+s","c+s+m","s+m"))
 dev.off()
 extraconfig=list()
@@ -53,7 +53,7 @@ extraconfig[5]=slice[4]
 extraconfig[6]=cs[4]
 extraconfig[7]=csm[4]
 extraconfig[8]=sm[4]
-png("/Users/student/Desktop/result/extraconfig.png")
+png("extraconfig.png",width = 800)
 boxplot (extraconfig,data=extraconfig, main="numbers of extraconfig", xlab="Number of extraconfig", ylab="Differnt experiment", names=c("noop","compute","minimize","c+m","slice","c+s","c+s+m","s+m"))
 dev.off()
 extracompute=list()
@@ -65,7 +65,7 @@ extracompute[5]=slice[5]
 extracompute[6]=cs[5]
 extracompute[7]=csm[5]
 extracompute[8]=sm[5]
-png("/Users/student/Desktop/result/extracompute.png")
+png("extracompute.png",width = 800)
 boxplot (extracompute,data=extracompute, main="numbers of extracompute", xlab="Number of extracompute", ylab="Differnt experiment", names=c("noop","compute","minimize","c+m","slice","c+s","c+s+m","s+m"))
 dev.off()
 addacl<-subset(mydata,scenario=="add-acl")
@@ -85,7 +85,7 @@ result[5]<-rmneighbor[1]
 result[6]<-rmnetwork[1]
 result[7]<-rmredistribute[1]
 result[8]<-rmstatic[1]
-png("/Users/student/Desktop/result/examples(network).png")
+png("examples(network).png",width = 800)
 boxplot (result,data=result, main="numbers of examples", xlab="Number of examples", ylab="Differnt network", names=c("add-acl","routemap","interface","original","neighbor","network","redistribute","static"))
 dev.off()
 result[1]<-addacl[2]
@@ -96,7 +96,7 @@ result[5]<-rmneighbor[2]
 result[6]<-rmnetwork[2]
 result[7]<-rmredistribute[2]
 result[8]<-rmstatic[2]
-png("/Users/student/Desktop/result/foundpred(network).png")
+png("foundpred(network).png",width = 800)
 boxplot (result,data=result, main="numbers of foundpred", xlab="Number of foundpred", ylab="Differnt network", names=c("add-acl","routemap","interface","original","neighbor","network","redistribute","static"))
 dev.off()
 result[1]<-addacl[3]
@@ -107,7 +107,7 @@ result[5]<-rmneighbor[3]
 result[6]<-rmnetwork[3]
 result[7]<-rmredistribute[3]
 result[8]<-rmstatic[3]
-png("/Users/student/Desktop/result/unfoundpred(network).png")
+png("unfoundpred(network).png",width = 800)
 boxplot (result,data=result, main="numbers of unfoundpred", xlab="Number of unfoundpred", ylab="Differnt network", names=c("add-acl","routemap","interface","original","neighbor","network","redistribute","static"))
 dev.off()
 result[1]<-addacl[4]
@@ -118,7 +118,7 @@ result[5]<-rmneighbor[4]
 result[6]<-rmnetwork[4]
 result[7]<-rmredistribute[4]
 result[8]<-rmstatic[4]
-png("/Users/student/Desktop/result/extraconfig(network).png")
+png("extraconfig(network).png",width = 800)
 boxplot (result,data=result, main="numbers of extraconfig", xlab="Number of extraconfig", ylab="Differnt network", names=c("add-acl","routemap","interface","original","neighbor","network","redistribute","static"))
 dev.off()
 result[1]<-addacl[5]
@@ -129,7 +129,7 @@ result[5]<-rmneighbor[5]
 result[6]<-rmnetwork[5]
 result[7]<-rmredistribute[5]
 result[8]<-rmstatic[5]
-png("/Users/student/Desktop/result/extracompute(network).png")
+png("extracompute(network).png",width = 800)
 boxplot (result,data=result, main="numbers of extracompute", xlab="Number of extracompute", ylab="Differnt network", names=c("add-acl","routemap","interface","original","neighbor","network","redistribute","static"))
 dev.off()
 
