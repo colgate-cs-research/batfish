22bfc06271cdfa6a5e60dfb5694650904f7fa848
Xiaolin Sun <xsun@colgate.edu>
2018-07-12 13:02:45 -0400

    update cmdfile

--- test_rigs/faultloc-examples/fattree/original/policies/reach.cmd
+++ test_rigs/faultloc-examples/fattree/original/policies/reach.cmd
@@ -1,1 +1,1 @@
-get smt-reachability ingressNodeRegex="core-2", finalNodeRegex="edge-6", srcIps=['70.0.2.0/32'], dstIps=['70.0.6.0/32'], failures=1
+get smt-reachability ingressNodeRegex="core-2", finalNodeRegex="edge-6", srcIps=['70.0.0.0/32'], dstIps=['70.0.6.0/32'], failures=1
