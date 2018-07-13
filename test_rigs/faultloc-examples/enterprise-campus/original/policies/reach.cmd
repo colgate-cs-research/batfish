1e7752ee3a47a51166c8337038e029e9e8f77d64
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2018-07-13 09:06:00 -0400

    Changed enterprise campus configs to only use OSPF

---
+++ test_rigs/faultloc-examples/enterprise-campus/original/policies/reach.cmd
@@ -0,0 +1,6 @@
+get smt-reachability ingressNodeRegex="E", finalNodeRegex="F", srcIps=['201.0.0.0/24'], dstIps=['202.0.0.0/24'], failures=1
+get smt-reachability ingressNodeRegex="E", finalNodeRegex="G", srcIps=['201.0.0.0/24'], dstIps=['203.0.0.0/24'], failures=1
+get smt-reachability ingressNodeRegex="F", finalNodeRegex="E", srcIps=['202.0.0.0/24'], dstIps=['201.0.0.0/24'], failures=1
+get smt-reachability ingressNodeRegex="F", finalNodeRegex="G", srcIps=['202.0.0.0/24'], dstIps=['203.0.0.0/24'], failures=1
+get smt-reachability ingressNodeRegex="G", finalNodeRegex="E", srcIps=['203.0.0.0/24'], dstIps=['201.0.0.0/24'], failures=1
+get smt-reachability ingressNodeRegex="G", finalNodeRegex="F", srcIps=['203.0.0.0/24'], dstIps=['202.0.0.0/24'], failures=1
