51574eb152bbcbcd90298e52c857e01747ac7384
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2018-07-26 16:34:20 -0400

    Clean-up configs for motivation example (still broken)

---
+++ test_rigs/faultloc-examples/motivate-example/original/policies/reach.cmd
@@ -0,0 +1,4 @@
+get smt-reachability ingressNodeRegex="r1", finalNodeRegex="r3", srcIps=['11.0.0.0/16'], dstIps=['13.0.0.0/17'], failures=1
+get smt-reachability ingressNodeRegex="r1", finalNodeRegex="r3", srcIps=['11.0.0.0/16'], dstIps=['13.0.128.0/17'], failures=1
+get smt-reachability ingressNodeRegex="r2", finalNodeRegex="r3", srcIps=['12.0.0.0/16'], dstIps=['13.0.0.0/17'], failures=1
+get smt-reachability ingressNodeRegex="r2", finalNodeRegex="r3", srcIps=['12.0.0.0/16'], dstIps=['13.0.128.0/17'], failures=1
