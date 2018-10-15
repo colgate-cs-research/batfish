8b851ff7b091fc11786e7035ff2b2761480f48c5
rctlynx <ruchitshrestha98@gmail.com>
2018-10-15 14:27:06 -0400

    script for marco_mus experiments

---
+++ test_rigs/faultloc-examples/marco_experiments.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#Determine path to scripts
+if [[ $(uname) == "Darwin" ]]; then
+    SCRIPTPATH=`greadlink -f $0`
+else
+    SCRIPTPATH=`readlink -f $0`
+fi
+BASEDIR="`dirname $SCRIPTPATH`"
+
+# Set results directory
+OUTPUTDIR="$BASEDIR/../../marco_output"
+mkdir -p $OUTPUTDIR
+LOGDIR="$OUTPUTDIR/logs"
+mkdir -p $LOGDIR
+RESULTDIR="$OUTPUTDIR/result"
+mkdir -p $RESULTDIR
+
+# Configure experimental setups
+declare -A OPTIONS=(["i"]="musIntersect" \
+                    ["u"]="musUnion" \
+                    ["n"]="noNegateProperty")
+declare -a SETUPS=("i" "in" "u" "un")
+
+MAX_MUS_COUNTS=(25 50 75 100)
+# Clean-up from last experiment
+rm -rf $BASEDIR/containers_marco
+
+#Running all setups
+for i in "${MAX_MUS_COUNTS[@]}"; do
+    for SETUP in "${SETUPS[@]}"; do
+
+        # Prepare options file
+        echo "add-batfish-option numIters 20" > $BASEDIR/custom-options
+        echo "add-batfish-option mus" $i >>$BASEDIR/custom-options
+        for (( i=0; i<${#SETUP}; i++ )); do
+            CHAR=${SETUP:$i:1}
+            OPTION=${OPTIONS[$CHAR]}
+            echo "add-batfish-option $OPTION" >> $BASEDIR/custom-options
+        done
+        #TODO: Find out what purpose this serves.
+        if [-z $SETUP]; then
+            SETUP="noop"
+        fi
+
+        # Run experiment
+        $BASEDIR/run_all.sh 2>&1 | tee $LOGDIR/SETUP.log
+
+    done
+done
+
+#Aggregate results
+python3 $BASEDIR/aggregate.py -path $OUTPUTDIR
+
+#Render graphs
+cd $OUTPUTDIR
+Rscript $BASEDIR/generategraph.R
+cd $BASEDIR
+
+#Clean up
+
+rm $BASEDIR/custom-options
