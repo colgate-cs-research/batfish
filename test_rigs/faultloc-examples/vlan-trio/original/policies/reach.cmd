7cd1506f2b611c859f714a3e3f73df56a27d5662
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2019-05-28 22:46:52 -0400

    Added vlan-trio faultloc example

---
+++ test_rigs/faultloc-examples/vlan-trio/original/policies/reach.cmd
@@ -0,0 +1,2 @@
+get smt-reachability ingressNodeRegex="edge1",finalNodeRegex="core",srcIps=['192.168.1.1/24'],dstIps=['10.0.50.1/24'],failures=0
+get smt-reachability ingressNodeRegex="edge1",finalNodeRegex="core",srcIps=['192.168.1.1/24'],dstIps=['10.10.20.1/24'],failures=0
