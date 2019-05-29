0b3a827d86897c8b6c76222f556322e730e4f094
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2019-05-28 22:54:02 -0400

    Additional policy for vlan-trio

--- test_rigs/faultloc-examples/vlan-trio/original/policies/reach.cmd
+++ test_rigs/faultloc-examples/vlan-trio/original/policies/reach.cmd
@@ -1,2 +1,3 @@
 get smt-reachability ingressNodeRegex="edge1",finalNodeRegex="core",srcIps=['192.168.1.1/24'],dstIps=['10.0.50.1/24'],failures=0
 get smt-reachability ingressNodeRegex="edge1",finalNodeRegex="core",srcIps=['192.168.1.1/24'],dstIps=['10.10.20.1/24'],failures=0
+get smt-reachability ingressNodeRegex="core",finalNodeRegex="core",srcIps=['10.0.50.1/24'],dstIps=['172.16.99.1/32'],failures=0
