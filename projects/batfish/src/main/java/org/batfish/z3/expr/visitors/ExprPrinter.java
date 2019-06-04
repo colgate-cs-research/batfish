ad5b0636d0fb9607ef09bb756968864818eb17f0
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2019-06-03 21:25:26 -0400

    Remove unused debug output

--- projects/batfish/src/main/java/org/batfish/z3/expr/visitors/ExprPrinter.java
+++ projects/batfish/src/main/java/org/batfish/z3/expr/visitors/ExprPrinter.java
@@ -3,7 +3,6 @@ package org.batfish.z3.expr.visitors;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import java.util.List;
-import java.util.Arrays;
 import org.batfish.z3.expr.AndExpr;
 import org.batfish.z3.expr.BasicRuleStatement;
 import org.batfish.z3.expr.BasicStateExpr;
@@ -39,9 +38,6 @@ public class ExprPrinter implements ExprVisitor, VoidStatementVisitor {
   public static String print(Expr expr) {
     ExprPrinter printer = new ExprPrinter();
     expr.accept(printer);
-    List<Expr> list = Arrays.asList(expr);
-    //System.out.println("printCollapsedComplexExpr");
-    //printer.printCollapsedComplexExpr(list);
     return printer._sb.toString();
   }
 
