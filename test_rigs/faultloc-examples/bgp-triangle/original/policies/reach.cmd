04e946cb56ecacb4d040b8298274505a3e56595f
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2019-06-05 19:52:29 -0400

    Fix policies files for faultloc-examples

--- test_rigs/faultloc-examples/bgp-triangle/original/policies/reach.cmd
+++ test_rigs/faultloc-examples/bgp-triangle/original/policies/reach.cmd
@@ -1,6 +1,6 @@
-get smt-reachability ingressNodeRegex="d",finalNodeRegex="C",srcIps=['12.0.0.0/8'],dstIps=['13.0.0.0/8'],failures=1
-get smt-reachability ingressNodeRegex="c",finalNodeRegex="D",srcIps=['13.0.0.0/8'],dstIps=['12.0.0.0/8'],failures=1
-get smt-reachability ingressNodeRegex="d",finalNodeRegex="B",srcIps=['12.0.0.0/8'],dstIps=['11.0.0.0/8'],failures=1
-get smt-reachability ingressNodeRegex="b",finalNodeRegex="D",srcIps=['11.0.0.0/8'],dstIps=['12.0.0.0/8'],failures=1
-get smt-reachability ingressNodeRegex="c",finalNodeRegex="B",srcIps=['13.0.0.0/8'],dstIps=['11.0.0.0/8'],failures=1
-get smt-reachability ingressNodeRegex="b",finalNodeRegex="C",srcIps=['11.0.0.0/8'],dstIps=['13.0.0.0/8'],failures=1
+get smt-reachability ingressNodeRegex="d",finalNodeRegex="c",srcIps=['12.0.0.0/8'],dstIps=['13.0.0.0/8'],failures=1
+get smt-reachability ingressNodeRegex="c",finalNodeRegex="d",srcIps=['13.0.0.0/8'],dstIps=['12.0.0.0/8'],failures=1
+get smt-reachability ingressNodeRegex="d",finalNodeRegex="b",srcIps=['12.0.0.0/8'],dstIps=['11.0.0.0/8'],failures=1
+get smt-reachability ingressNodeRegex="b",finalNodeRegex="d",srcIps=['11.0.0.0/8'],dstIps=['12.0.0.0/8'],failures=1
+get smt-reachability ingressNodeRegex="c",finalNodeRegex="b",srcIps=['13.0.0.0/8'],dstIps=['11.0.0.0/8'],failures=1
+get smt-reachability ingressNodeRegex="b",finalNodeRegex="c",srcIps=['11.0.0.0/8'],dstIps=['13.0.0.0/8'],failures=1
--- test_rigs/faultloc-examples/enterprise-campus/original/policies/reach.cmd
+++ test_rigs/faultloc-examples/enterprise-campus/original/policies/reach.cmd
@@ -1,6 +1,6 @@
-get smt-reachability ingressNodeRegex="e",finalNodeRegex="F",srcIps=['201.0.0.0/24'],dstIps=['202.0.0.0/24'],failures=1
-get smt-reachability ingressNodeRegex="e",finalNodeRegex="G",srcIps=['201.0.0.0/24'],dstIps=['203.0.0.0/24'],failures=1
-get smt-reachability ingressNodeRegex="f",finalNodeRegex="E",srcIps=['202.0.0.0/24'],dstIps=['201.0.0.0/24'],failures=1
-get smt-reachability ingressNodeRegex="f",finalNodeRegex="G",srcIps=['202.0.0.0/24'],dstIps=['203.0.0.0/24'],failures=1
-get smt-reachability ingressNodeRegex="g",finalNodeRegex="E",srcIps=['203.0.0.0/24'],dstIps=['201.0.0.0/24'],failures=1
-get smt-reachability ingressNodeRegex="g",finalNodeRegex="F",srcIps=['203.0.0.0/24'],dstIps=['202.0.0.0/24'],failures=1
+get smt-reachability ingressNodeRegex="e",finalNodeRegex="f",srcIps=['201.0.0.0/24'],dstIps=['202.0.0.0/24'],failures=1
+get smt-reachability ingressNodeRegex="e",finalNodeRegex="g",srcIps=['201.0.0.0/24'],dstIps=['203.0.0.0/24'],failures=1
+get smt-reachability ingressNodeRegex="f",finalNodeRegex="e",srcIps=['202.0.0.0/24'],dstIps=['201.0.0.0/24'],failures=1
+get smt-reachability ingressNodeRegex="f",finalNodeRegex="g",srcIps=['202.0.0.0/24'],dstIps=['203.0.0.0/24'],failures=1
+get smt-reachability ingressNodeRegex="g",finalNodeRegex="e",srcIps=['203.0.0.0/24'],dstIps=['201.0.0.0/24'],failures=1
+get smt-reachability ingressNodeRegex="g",finalNodeRegex="f",srcIps=['203.0.0.0/24'],dstIps=['202.0.0.0/24'],failures=1
