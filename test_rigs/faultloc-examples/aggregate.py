064e097546359f84f11a11acf9a2834033a47580
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2019-06-05 19:53:02 -0400

    Update aggregate script to account for new containers dir structure

--- test_rigs/faultloc-examples/aggregate.py
+++ test_rigs/faultloc-examples/aggregate.py
@@ -1,62 +1,73 @@
 #!/usr/bin/env python3
+
 import os
 import csv
 import argparse
 
-parser = argparse.ArgumentParser(description='Aggregate experiment output')
-parser.add_argument('-path', dest='path', action='store', required=True,
-                   help='path containing experiment output')
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description='Aggregate experiment output')
+    parser.add_argument('-c','--containers', dest='path', action='store', 
+            required=True, help='Path to containers directory')
+    args = parser.parse_args()
 
-top_level_dir = os.path.join(args.path, "result")
-masterpath = os.path.join(args.path, "master.csv")
+    #top_level_dir = os.path.join(args.path, "result")
+    masterpath = os.path.join(args.path, "master.csv")
 
-# Open master file
+    # Open master file
+    masterfile = open(masterpath,'w')
+    writer = csv.writer(masterfile)
+    writer.writerow(["examples","foundpreds","unfoundpreds","extraconfigpred",
+            "extracomputepred","includecomputable","notnegating","minimize",
+            "slice","unfoundpred","slicetime","minimizationtime","checktime",
+            "numMUSGenerated","musGenTimeElapsed","experiment","network",
+            "scenario"])
 
-f = open(masterpath,'w')
-writer=csv.writer(f)
-writer.writerow(["examples","foundpreds","unfoundpreds","extraconfigpred","extracomputepred","includecomputable","notnegating","minimize","slice","unfoundpred","slicetime","minimizationtime","checktime","numMUSGenerated","musGenTimeElapsed","experiment","network","scenario"])
-# List every experiment
-experiments = os.listdir(top_level_dir)
-for experiment in experiments:
-    # Ignore hidden directories
-    if experiment.startswith('.'):
-        continue
-    experiment_dir = os.path.join(top_level_dir, experiment)
-
-
-    # List every network
-    networks = os.listdir(experiment_dir)
-    for network in networks:
-        # Ignore hidden directories
-        if network.startswith('.'):
-            continue
-        network_dir = os.path.join(experiment_dir, network)
-    
-        # List every scenario
-        scenarios = os.listdir(os.path.join(network_dir, 'testrigs'))
-        for scenario in scenarios:
-            # Ignore hidden directories
-            if scenario.startswith('.'):
+    if os.path.exists(os.path.join(args.path, "network_ids")):
+        process_experiment(None, args.path, writer)
+    else:
+        for experiment in os.listdir(args.path):
+            if experiment.startswith('.'):
                 continue
-            try:
-                scenario_file = os.path.join(network_dir, 'testrigs', scenario, 'testrig', 'experiment.csv')
-                test = open(scenario_file,'r')
-                readFile= csv.reader(test)
-                for row in readFile:
-                    if row[0].isdigit():
-                        row.append(experiment)
-                        row.append(network)
-                        row.append(scenario)
-                        writer.writerow(row)
-  
-                test.close()
-            except OSError:
-                pass
-            # TODO: Open scenario file
-            # TODO: For each line in scenario file
-            # TODO: Add experiment, network, and scenario to line
-            # TODO: Add line to master file
+            process_experiment(experiment, os.path.join(args.path, 
+                    experiment), writer)
+    masterfile.close()
 
-# Close master file
-f.close()
+def process_experiment(experiment_name, base_dir, writer):
+    # Process every network
+    ids_dir = os.path.join(base_dir, "network_ids")
+    for id_filename in os.listdir(ids_dir):
+        with open(os.path.join(ids_dir, id_filename)) as id_file:
+            network_id = id_file.read().strip()
+        network_name = id_filename[:-3]
+        process_network(experiment_name, network_name, 
+                os.path.join(base_dir, network_id), writer)
+
+def process_network(experiment_name, network_name, base_dir, writer):
+    # Process every snapshot
+    ids_dir = os.path.join(base_dir, "snapshot_ids")
+    for id_filename in os.listdir(ids_dir):
+        with open(os.path.join(ids_dir, id_filename)) as id_file:
+            snapshot_id = id_file.read().strip()
+        snapshot_name = id_filename[:-3]
+        process_snapshot(experiment_name, network_name, snapshot_name,
+                os.path.join(base_dir, "snapshots", snapshot_id), writer)
+
+def process_snapshot(experiment_name, network_name, snapshot_name, base_dir,
+        writer):
+    filepath = os.path.join(base_dir, "output", "experiment.csv")
+    print("%s %s %s" % (experiment_name, network_name, snapshot_name))
+    print("\t%s" % (base_dir))
+    try:
+        with open(filepath, "r") as experiment_file:
+            reader = csv.reader(experiment_file)
+            for row in reader:
+                if row[0].isdigit():
+                    row.append(experiment_name)
+                    row.append(network_name)
+                    row.append(snapshot_name)
+                    writer.writerow(row)
+    except OSError:
+        pass
+
+if __name__ == "__main__":
+    main()
