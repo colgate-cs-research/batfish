beb3088eac74fb501c57ac77067116c579e53689
Victor Heorhiadi <progwriter@users.noreply.github.com>
2019-06-10 09:13:29 -0700

    Towards proper support of link-local addresses (#4041)

--- projects/minesweeper/src/main/java/org/batfish/minesweeper/smt/PropertyAdder.java
+++ projects/minesweeper/src/main/java/org/batfish/minesweeper/smt/PropertyAdder.java
@@ -140,7 +140,7 @@ class PropertyAdder {
           // Also reachable if connected route and we use it despite not forwarding
           if (r != null) {
             BitVecExpr dstIp = _encoderSlice.getSymbolicPacket().getDstIp();
-            BitVecExpr ip = ctx.mkBV(ge.getStart().getAddress().getIp().asLong(), 32);
+            BitVecExpr ip = ctx.mkBV(ge.getStart().getConcreteAddress().getIp().asLong(), 32);
             BoolExpr reach = ctx.mkAnd(r.getPermitted(), ctx.mkEq(dstIp, ip));
             isAbsorbed = ctx.mkOr(isAbsorbed, reach);
           }
@@ -352,7 +352,7 @@ class PropertyAdder {
           // Also reachable if connected route and we use it despite not forwarding
           if (r != null) {
             BitVecExpr dstIp = _encoderSlice.getSymbolicPacket().getDstIp();
-            BitVecExpr ip = ctx.mkBV(ge.getStart().getAddress().getIp().asLong(), 32);
+            BitVecExpr ip = ctx.mkBV(ge.getStart().getConcreteAddress().getIp().asLong(), 32);
             BoolExpr reach = ctx.mkAnd(r.getPermitted(), ctx.mkEq(dstIp, ip));
             isAbsorbed = ctx.mkOr(isAbsorbed, reach);
           }
@@ -426,7 +426,7 @@ class PropertyAdder {
           // if connected route and we use it despite not forwarding
           if (r != null) {
             BitVecExpr dstIp = _encoderSlice.getSymbolicPacket().getDstIp();
-            BitVecExpr ip = ctx.mkBV(ge.getStart().getAddress().getIp().asLong(), 32);
+            BitVecExpr ip = ctx.mkBV(ge.getStart().getConcreteAddress().getIp().asLong(), 32);
             BoolExpr reach = ctx.mkAnd(r.getPermitted(), ctx.mkEq(dstIp, ip));
             isAbsorbed = ctx.mkOr(isAbsorbed, reach);
           }
