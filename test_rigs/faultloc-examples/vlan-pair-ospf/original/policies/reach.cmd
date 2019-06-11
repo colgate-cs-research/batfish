5267a2f207600def03a597566ffe0dc718f5d1da
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2019-06-11 13:16:46 -0400

    Fixed bug in vlan-pair-ospf example

--- test_rigs/faultloc-examples/vlan-pair-ospf/original/policies/reach.cmd
+++ test_rigs/faultloc-examples/vlan-pair-ospf/original/policies/reach.cmd
@@ -1,1 +1,4 @@
+get smt-reachability ingressNodeRegex="a",finalNodeRegex="b",srcIps=['10.0.30.0/24'],dstIps=['10.0.50.0/24']
 get smt-reachability ingressNodeRegex="a",finalNodeRegex="b",srcIps=['10.0.40.0/24'],dstIps=['10.0.50.0/24']
+get smt-reachability ingressNodeRegex="b",finalNodeRegex="a",srcIps=['10.0.50.0/24'],dstIps=['10.0.30.0/24']
+get smt-reachability ingressNodeRegex="b",finalNodeRegex="a",srcIps=['10.0.50.0/24'],dstIps=['10.0.40.0/24']
