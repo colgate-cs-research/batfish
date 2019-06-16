a0475bc491adbda658e0a8044adda43a1d6d90f5
Xiaolin Sun <xsun@colgate.edu>
2019-06-16 19:31:11 -0400

    remove improper testcase and add mofy case

---
+++ test_rigs/faultloc-examples/fattree/mofy-subnet/policies/reach.cmd
@@ -0,0 +1,2 @@
+get smt-reachability ingressNodeRegex="edge-6",finalNodeRegex="edge-10",srcIps=['70.0.6.0/30'],dstIps=['70.0.10.0/30'],failures=1
+get smt-reachability ingressNodeRegex="edge-10",finalNodeRegex="edge-6",srcIps=['70.0.10.0/30'],dstIps=['70.0.6.0/30'],failures=1
