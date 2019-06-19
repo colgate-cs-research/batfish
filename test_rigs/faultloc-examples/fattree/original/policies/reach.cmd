1fe8f240926dbef2a8edc208a54ab50831c4ddb8
Xiaolin Sun <xsun@colgate.edu>
2019-06-19 12:00:59 -0400

    add mofy change to fattree-4

--- test_rigs/faultloc-examples/fattree/original/policies/reach.cmd
+++ test_rigs/faultloc-examples/fattree/original/policies/reach.cmd
@@ -1,2 +1,6 @@
 get smt-reachability ingressNodeRegex="edge-6",finalNodeRegex="edge-10",srcIps=['70.0.6.0/30'],dstIps=['70.0.10.0/30'],failures=0
 get smt-reachability ingressNodeRegex="edge-10",finalNodeRegex="edge-6",srcIps=['70.0.10.0/30'],dstIps=['70.0.6.0/30'],failures=0
+get smt-reachability ingressNodeRegex="edge-10",finalNodeRegex="edge-11",srcIps=['70.0.10.0/31'],dstIps=['70.0.11.2/31']
+get smt-reachability ingressNodeRegex="edge-10",finalNodeRegex="edge-11",srcIps=['70.0.10.2/31'],dstIps=['70.0.11.2/31']
+get smt-reachability ingressNodeRegex="edge-15",finalNodeRegex="edge-14",srcIps=['70.0.15.2/31'],dstIps=['70.0.14.2/31']
+get smt-reachability ingressNodeRegex="edge-11",finalNodeRegex="edge-14",srcIps=['70.0.11.2/31'],dstIps=['70.0.14.2/31']
