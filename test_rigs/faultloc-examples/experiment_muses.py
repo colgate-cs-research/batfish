738d1c4542b1d497c15668d8146a461c947aeb4a
Xiaolin Sun <xsun@colgate.edu>
2019-06-26 16:12:59 -0400

    update experiment_muses.py to enable various muses, failures and polices

--- test_rigs/faultloc-examples/experiment_muses.py
+++ test_rigs/faultloc-examples/experiment_muses.py
@@ -15,21 +15,28 @@ def main():
     #regardless of whether failsets are intersected or unioned
     parser.add_argument('-s', '--iScenario', dest='i_scenario', action='store_true', help='Only consider predicates that are candidates for each policy')
     #Do not aggregate results to networks, instead have resulting candidates as faults for all scenarios. (same for all scenarios)
+    parser.add_argument('-nm', '--number of mus', dest='mus', action='store_true', help='numbers of muses to consider')
+    parser.add_argument('-nf', '--number of failure', dest='failure', action='store_true', help='number of failures to consider')
+    parser.add_argument('-np', '--number of policies', dest='policies', action='store_true', help='number of policies to consider')
+    parser.add_argument('-pp', '--printPerPolicy', dest='print_per_policy', action='store_true', help='Print results per policy instead of per-scenario')
     args = parser.parse_args()
 
     identifier = "_"
     identifier += "i" if args.i_fail else "u"
     identifier += "i" if args.i_policy else "u"
     identifier += "i" if args.i_scenario else "u"
-
+    identifier += "various_mus" if args.mus else ""
+    identifier += "various_failure" if args.failure else ""
+    identifier += "various_policies" if args.policies else ""
+    
     masterfile_name = "master_mus" + identifier + ".csv"
+    print(masterfile_name)
     #top_level_dir = os.path.join(args.path, "result")
     masterpath = os.path.join(args.path, masterfile_name)
-
     masterfile = open(masterpath, 'w')
     writer = csv.writer(masterfile)
-    writer.writerow(["found_preds_count","missed_preds_count","extra_count","missed_preds", "network", "scenario"])
-
+    writer.writerow(["found_preds_count","missed_preds_count","extra_count","missed_preds", "network", "scenario", "num_mus", "num_failures", "num_policies"])
+        
     ids_dir = os.path.join(args.path, "network_ids")
     for id_filename in os.listdir(ids_dir):
         with open(os.path.join(ids_dir, id_filename)) as id_file:
@@ -50,33 +57,95 @@ def process_network(network, network_dir, writer, args):
                 snapshot_id = id_file.read().strip()
             snapshot_name = id_filename[:-3]
             snapshot_dir = os.path.join(network_dir, "snapshots", snapshot_id)
-            faulty_preds, snapshot_candidates = process_snapshot(snapshot_dir, args)
-            found_count, missed_count, missed_preds, extra_count = analyze_candidates(snapshot_candidates, faulty_preds)
-            if found_count == 0 == missed_count:
-                continue # No use writing out where there aren't any faults
-            writer.writerow(
-                [str(found_count),
-                str(missed_count),
-                str(extra_count),
-                str(missed_preds),
-                network,
-                snapshot_name]
-            )
+            if args.mus == args.failure == args.policies == False:
+                faulty_preds, snapshot_candidates, num_mus ,num_failures, num_policies= process_snapshot_num(snapshot_dir, writer, args)
+                found_count, missed_count, missed_preds, extra_count = analyze_candidates(snapshot_candidates, faulty_preds)
+                if (not args.print_per_policy):
+                    writer.writerow(
+                        [str(found_count),
+                        str(missed_count),
+                        str(extra_count),
+                        str(missed_preds),
+                        network,
+                        snapshot_name,
+                        num_mus,
+                        num_failures,
+                        num_policies]
+                    )
+            else:
+                nums = [0,1,2,4,8,16,32,64]
+                for i, num in enumerate(nums):
+                    if i == 0:
+                        continue
+                    faulty_preds, snapshot_candidates,num_mus, num_failures, num_policies = process_snapshot_num(snapshot_dir, writer, args, num)
+                    found_count, missed_count, missed_preds, extra_count = analyze_candidates(snapshot_candidates, faulty_preds)
+                    if ((args.mus and num_mus>=nums[i-1])or(args.failure and num_failures>=nums[i-1])or(args.policies and num_policies>=nums[i-1])):
+                        if (not args.print_per_policy):
+                            writer.writerow(
+                                [str(found_count),
+                                str(missed_count),
+                                str(extra_count),
+                                str(missed_preds),
+                                network,
+                                snapshot_name,
+                                num_mus,
+                                num_failures,
+                                num_policies])
 
-def process_snapshot(snapshot_dir, args):
+
+def process_snapshot_num(snapshot_dir, writer, args, num = 1000000):
     output_dir = os.path.join(snapshot_dir, "output")
     policy_to_candidates = dict()
+    policy_to_failcount = dict()
+    policy_to_muscount = dict()
     for filename in os.listdir(output_dir):
         if filename.startswith('mus_bulk'):
             mus_path = os.path.join(output_dir, filename)
-            policy, candidates = process_mus_intersect(mus_path) if args.i_fail else process_mus(mus_path)
-            if policy not in policy_to_candidates:
-                policy_to_candidates[policy] = candidates
+            if not args.mus: 
+                policy, candidates, num_mus= process_mus_intersect(mus_path) if args.i_fail else process_mus(mus_path)
             else:
-                if args.i_policy:
-                    policy_to_candidates[policy].intersection_update(candidates) #TODO: Check if dict updated
+                policy, candidates, num_mus= process_mus_intersect_num(mus_path, num) if args.i_fail else process_mus_num(mus_path, num)
+                
+            if policy not in policy_to_failcount:
+                policy_to_failcount[policy] = 0
+                policy_to_muscount[policy] = []
+            if (not args.failure or policy_to_failcount[policy] < num):
+                policy_to_failcount[policy] = policy_to_failcount[policy]+1
+            else:
+                continue
+
+            if (not args.policies or len(policy_to_candidates) < num):
+                policy_to_muscount[policy].append(num_mus)
+                if policy not in policy_to_candidates:
+                    policy_to_candidates[policy] = candidates
                 else:
-                    policy_to_candidates[policy].update(candidates)
+                    if args.i_policy:
+                        policy_to_candidates[policy].intersection_update(candidates) #TODO: Check if dict updated
+                    else:
+                        policy_to_candidates[policy].update(candidates)
+
+    faultloc_filepath = os.path.join(snapshot_dir, "input", "faultloc")
+    faulty_preds = process_faultloc_file (faultloc_filepath)
+
+    if (args.print_per_policy):
+        for policy in policy_to_candidates.keys():
+            policy_candidates = policy_to_candidates[policy]
+            found_count, missed_count, missed_preds, extra_count = analyze_candidates(policy_candidates, faulty_preds)
+            num_mus = min(policy_to_muscount[policy])
+            num_failures = policy_to_failcount[policy]
+            if ((args.mus)or(args.failure and num_failures>=num)or (not args.mus and not args.failure)):
+                writer.writerow(
+                            [str(found_count),
+                            str(missed_count),
+                            str(extra_count),
+                            str(missed_preds),
+                            "",
+                            "",
+                            num_mus,
+                            num_failures,
+                            policy]
+                        )
+        return set(), set(), 0, 0, 0
 
     snapshot_candidates = set()
     first = True
@@ -91,10 +160,9 @@ def process_snapshot(snapshot_dir, args):
         else:
             snapshot_candidates.intersection_update(policy_to_candidates[policy])
 
-    faultloc_filepath = os.path.join(snapshot_dir, "input", "faultloc")
-    faulty_preds = process_faultloc_file (faultloc_filepath)
+        #min([min(mus_count) for mus_count in policy_to_muscount.values()])
 
-    return faulty_preds, snapshot_candidates
+    return faulty_preds, snapshot_candidates, min([min(mus_count) for mus_count in policy_to_muscount.values()]), max(policy_to_failcount.values()), len(policy_to_candidates)
 
 def process_faultloc_file(filepath):
     faulty_preds = set()
@@ -114,16 +182,34 @@ def process_faultloc_file(filepath):
 
 def process_mus(mus_file_path):
     preds= set()
+    count = 0
 
     with open(mus_file_path, "r") as mus_file:
         for mus in mus_file:
+            count = count+1
             if mus.startswith('['): #first line is policy being checked
                 policy = mus
                 continue
             preds_list =  mus.split(',')
             for x in preds_list:
                 preds.add(x)
-    return policy,preds
+    return policy,preds,count-1
+
+def process_mus_num(mus_file_path, num):
+    preds= set()
+    count = 0
+    with open(mus_file_path, "r") as mus_file:
+        for mus in mus_file:
+            count = count+1
+            if mus.startswith('['): #first line is policy being checked
+                policy = mus
+                continue
+            preds_list =  mus.split(',')
+            for x in preds_list:
+                preds.add(x)
+            if count == (num+1):
+                break
+    return policy,preds,count-1
 
 def analyze_candidates(candidates, faults):
     found_count = len(faults.intersection(candidates))
@@ -137,8 +223,10 @@ def analyze_candidates(candidates, faults):
 def process_mus_intersect(mus_file_path):
     preds = set()
     first = True
+    count = 0
     with open(mus_file_path, "r") as mus_file:
         for mus in mus_file:
+            count = count+1
             if mus.startswith('['):
                 policy = mus
                 continue
@@ -148,8 +236,28 @@ def process_mus_intersect(mus_file_path):
                 first = False
             else:
                 preds = preds.intersection(preds_set)
+    return policy, preds, count-1
 
-    return policy, preds
+def process_mus_intersect_num(mus_file_path, num):
+    preds = set()
+    first = True
+    count = 0
+    with open(mus_file_path, "r") as mus_file:
+        for mus in mus_file:
+            count = count+1
+            if mus.startswith('['):
+                policy = mus
+                continue
+            preds_set = set(mus.split(','))
+            if (first):
+                preds = preds.union(preds_set)
+                first = False
+            else:
+                preds = preds.intersection(preds_set)
+            if count == (num+1):
+                break
+
+    return policy, preds, count-1
 
 if __name__ == '__main__':
     main()
