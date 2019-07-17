b2b0513ac086e4cd74a578eaaacb2fd4e7532209
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2019-07-17 10:32:25 -0400

    Switch maven to batch mode when building

--- tools/common.sh
+++ tools/common.sh
@@ -99,7 +99,7 @@ export -f batfish_build_all
 _batfish_build_all() {
    _pre_build || return 1
    cd "${PROJECTS_PATH}"
-   mvn --no-transfer-progress install || return 1
+   mvn -B install || return 1
 }
 export -f _batfish_build_all
 
