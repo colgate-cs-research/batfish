d48a6c8e711bfd2cca999f94e3058cd98c244562
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2020-05-26 14:48:49 -0400

    Clean-up script for analyzing saved MUSes (experiment_muses.py)

--- test_rigs/faultloc-examples/experiment_muses.py
+++ test_rigs/faultloc-examples/experiment_muses.py
@@ -37,6 +37,7 @@ def main():
     writer = csv.writer(masterfile)
     writer.writerow(["found_preds_count","missed_preds_count","extra_count","recall","precision","missed_preds", "extra_preds", "experiment","network", "scenario", "num_mus", "num_failures", "num_policies"])
 
+    # Process a single container directory or multiple container directories
     if os.path.exists(os.path.join(args.path, "network_ids")):
         process_experiment(args.path, writer, args)
     else:
@@ -46,11 +47,14 @@ def main():
                     or not os.path.isdir(experiment_dir)):
                 continue
             process_experiment(experiment, experiment_dir, writer, args)
-    
+   
     masterfile.close()
 
 def process_experiment(experiment_name, experiment_dir, writer, args):
+    print(experiment_name)
     ids_dir = os.path.join(experiment_dir, "network_ids")
+
+    # Process each network
     for id_filename in sorted(os.listdir(ids_dir)):
         with open(os.path.join(ids_dir, id_filename)) as id_file:
             network_id = id_file.read().strip()
@@ -63,59 +67,69 @@ def process_experiment(experiment_name, experiment_dir, writer, args):
 
 
 def process_network(experiment_name, network, network_dir, writer, args):
-
     snapshot_ids_dir = os.path.join(network_dir, "snapshot_ids")
-    if os.path.exists(snapshot_ids_dir):
-        for id_filename in sorted(os.listdir(snapshot_ids_dir)):
-            with open(os.path.join(snapshot_ids_dir, id_filename)) as id_file:
-                snapshot_id = id_file.read().strip()
-            snapshot_name = id_filename[:-3]
-            snapshot_dir = os.path.join(network_dir, "snapshots", snapshot_id)
-            if args.mus == args.failure == args.policies == False:
-                faulty_preds, snapshot_candidates, num_mus ,num_failures, num_policies= process_snapshot_num(experiment_name, network, snapshot_name, snapshot_dir, writer, args)
-                found_count, missed_count, missed_preds, extra_count, extra_preds = analyze_candidates(snapshot_candidates, faulty_preds)
-                if (not args.print_per_policy):
-                    writer.writerow(
-                        [str(found_count),
-                        str(missed_count),
-                        str(extra_count),
-                        str(found_count/(found_count+missed_count)),
-                        str(found_count/(found_count+extra_count)),
-                        str(missed_preds),
-                        str(extra_preds),
-                        experiment_name,
-                        network,
-                        snapshot_name,
-                        num_mus,
-                        num_failures,
-                        num_policies]
-                    )
-            else:
-                nums = [0,1,2,4,8,16,32,64]
-                for i, num in enumerate(nums):
-                    if i == 0:
-                        continue
-                    faulty_preds, snapshot_candidates,num_mus, num_failures, num_policies = process_snapshot_num(experiment_name, network, snapshot_name, snapshot_dir, writer, args, num)
-                    found_count, missed_count, missed_preds, extra_count, extra_preds = analyze_candidates(snapshot_candidates, faulty_preds)
-                    if ((args.mus and num_mus>=nums[i-1])or(args.failure and num_failures>=nums[i-1])or(args.policies and num_policies>=nums[i-1])):
-                        if (not args.print_per_policy):
-                            writer.writerow(
-                                [str(found_count),
-                                str(missed_count),
-                                str(extra_count),
-                                str(found_count/(found_count+missed_count)),
-                                str(found_count/(found_count+extra_count)),
-                                str(missed_preds),
-                                str(extra_preds),
-                                experiment_name,
-                                network,
-                                snapshot_name,
-                                num_mus,
-                                num_failures,
-                                num_policies])
+    if not os.path.exists(snapshot_ids_dir):
+        return
+    print(' ', network)
+
+    # Process each snapshot
+    for id_filename in sorted(os.listdir(snapshot_ids_dir)):
+        with open(os.path.join(snapshot_ids_dir, id_filename)) as id_file:
+            snapshot_id = id_file.read().strip()
+        snapshot_name = id_filename[:-3]
+        snapshot_dir = os.path.join(network_dir, "snapshots", snapshot_id)
+
+        # Compute stats for all MUSes, failures, and policies
+        if args.mus == args.failure == args.policies == False:
+            nums = [0, 10**6]
+        # Compute stats for varying numbers of MUSes, failures, or policies
+        else:
+            nums = [0,1,2,4,8,16,32,64]
+
+        # Compute stats each maximum number of MUSes, failures, or policies
+        for i, num in enumerate(nums):
+            if i == 0:
+                continue
+            (faulty_preds, snapshot_candidates, num_mus, num_failures, 
+                    num_policies) = process_snapshot_num(experiment_name, 
+                        network, snapshot_name, snapshot_dir, writer, args, 
+                        num)
+            found_count, missed_preds, extra_preds = analyze_candidates(
+                    snapshot_candidates, faulty_preds)
+            print('      found:', found_count, 'missed:', len(missed_preds), 
+                    'extra:', len(extra_preds))
+            if len(faulty_preds) == 0:
+                print('     extra:', '\n\t'.join(sorted(extra_preds)))
+            if (not args.print_per_policy and
+                    (args.mus == args.failure == args.policies == False
+                        or (args.mus and num_mus >= nums[i-1])
+                        or (args.failure and num_failures >= nums[i-1])
+                        or (args.policies and num_policie >= nums[i-1]))):
+                if (found_count == 0 and len(missed_preds) == 0):
+                    recall = 'NA'
+                else:
+                    recall = found_count / (found_count + len(missed_preds))
+                precision = found_count / (found_count + len(extra_preds))
+                print('      precision:', precision, 'recall:', recall)
+                writer.writerow(
+                    [str(found_count),
+                    str(len(missed_preds)),
+                    str(len(extra_preds)),
+                    str(recall),
+                    str(precision),
+                    str(';'.join(missed_preds)),
+                    str(';'.join(extra_preds)),
+                    experiment_name,
+                    network,
+                    snapshot_name,
+                    num_mus,
+                    num_failures,
+                    num_policies])
 
 
-def process_snapshot_num(experiment_name, network, snapshot, snapshot_dir, writer, args, num = 1000000):
+def process_snapshot_num(experiment_name, network, snapshot, snapshot_dir, 
+        writer, args, num=10**6):
+    print('   ', snapshot)
     output_dir = os.path.join(snapshot_dir, "output")
     policy_to_candidates = dict()
     policy_to_failcount = dict()
@@ -123,20 +137,21 @@ def process_snapshot_num(experiment_name, network, snapshot, snapshot_dir, write
     for filename in sorted(os.listdir(output_dir)):
         if filename.startswith('mus_bulk'):
             mus_path = os.path.join(output_dir, filename)
-            if not args.mus: 
-                policy, candidates, num_mus= process_mus_intersect(mus_path) if args.i_fail else process_mus(mus_path)
-            else:
-                policy, candidates, num_mus= process_mus_intersect_num(mus_path, num) if args.i_fail else process_mus_num(mus_path, num)
-                
-            if policy not in policy_to_failcount:
-                policy_to_failcount[policy] = 0
-                policy_to_muscount[policy] = []
-            if (not args.failure or policy_to_failcount[policy] < num):
-                policy_to_failcount[policy] = policy_to_failcount[policy]+1
+            policy, candidates, num_mus = (process_mus_intersect(mus_path, num) 
+                    if args.i_fail else process_mus(mus_path, num))
+            print("      policy:", policy, "cands:", len(candidates), 
+                    "mus:", num_mus)
+               
+            # Limit number of failures considered, if requested
+            if (not args.failure or policy_to_failcount.get(policy, 0) < num):
+                policy_to_failcount[policy] = policy_to_failcount.get(policy, 0) + 1
             else:
                 continue
 
+            # Limit number of MUSes considered, if requested
             if (not args.policies or len(policy_to_candidates) < num):
+                if policy not in policy_to_muscount:
+                    policy_to_muscount[policy] = []
                 policy_to_muscount[policy].append(num_mus)
                 if policy not in policy_to_candidates:
                     policy_to_candidates[policy] = candidates
@@ -152,16 +167,24 @@ def process_snapshot_num(experiment_name, network, snapshot, snapshot_dir, write
     if (args.print_per_policy):
         for policy in policy_to_candidates.keys():
             policy_candidates = policy_to_candidates[policy]
-            found_count, missed_count, missed_preds, extra_count, extra_preds = analyze_candidates(policy_candidates, faulty_preds)
+            found_count, missed_preds, extra_preds = analyze_candidates(
+                    policy_candidates, faulty_preds)
             num_mus = min(policy_to_muscount[policy])
             num_failures = policy_to_failcount[policy]
-            if ((args.mus)or(args.failure and num_failures>=num)or (not args.mus and not args.failure)):
+            if ((args.mus) or (args.failure and num_failures>=num)
+                    or (not args.mus and not args.failure)):
+                if (found_count == 0 and len(missed_preds) == 0):
+                    recall = 'NA'
+                else:
+                    recall = found_count / (found_count + len(missed_preds))
+                precision = found_count / (found_count + len(extra_preds))
+                print('      precision:', precision, 'recall:', recall)
                 writer.writerow(
                             [str(found_count),
-                            str(missed_count),
-                            str(extra_count),
-                            str(found_count/(found_count+missed_count)),
-                            str(found_count/(found_count+extra_count)),
+                            str(len(missed_preds)),
+                            str(len(extra_preds)),
+                            str(recall),
+                            str(precision),
                             str(missed_preds),
                             str(extra_preds),
                             experiment_name,
@@ -188,7 +211,9 @@ def process_snapshot_num(experiment_name, network, snapshot, snapshot_dir, write
 
         #min([min(mus_count) for mus_count in policy_to_muscount.values()])
 
-    return faulty_preds, snapshot_candidates, min([min(mus_count) for mus_count in policy_to_muscount.values()]), max(policy_to_failcount.values()), len(policy_to_candidates)
+    return (faulty_preds, snapshot_candidates, 
+            min([min(mus_count) for mus_count in policy_to_muscount.values()]), 
+            max(policy_to_failcount.values()), len(policy_to_candidates))
 
 def process_faultloc_file(filepath):
     faulty_preds = set()
@@ -206,29 +231,14 @@ def process_faultloc_file(filepath):
         pass
     return faulty_preds
 
-def process_mus(mus_file_path):
-    preds= set()
-    count = 0
-
-    with open(mus_file_path, "r") as mus_file:
-        for mus in mus_file:
-            count = count+1
-            if mus.startswith('['): #first line is policy being checked
-                policy = mus
-                continue
-            preds_list =  mus.strip('\n,').split(',')
-            for x in preds_list:
-                preds.add(x)
-    return policy,preds,count-1
-
-def process_mus_num(mus_file_path, num):
+def process_mus(mus_file_path, num=10**6):
     preds= set()
     count = 0
     with open(mus_file_path, "r") as mus_file:
         for mus in mus_file:
             count = count+1
             if mus.startswith('['): #first line is policy being checked
-                policy = mus
+                policy = mus.strip()
                 continue
             preds_list =  mus.strip('\n,').split(',')
             for x in preds_list:
@@ -239,33 +249,14 @@ def process_mus_num(mus_file_path, num):
 
 def analyze_candidates(candidates, faults):
     found_count = len(faults.intersection(candidates))
-    missed_count = len(faults) - found_count
     missed_preds = faults.difference(candidates)
-    extra_count = len(candidates) - found_count
+    assert(len(missed_preds) == (len(faults) - found_count))
     extra_preds = candidates.difference(faults)
+    assert(len(extra_preds) == (len(candidates) - found_count))
 
-    return found_count, missed_count, missed_preds, extra_count, extra_preds
+    return found_count, missed_preds, extra_preds
 
-
-def process_mus_intersect(mus_file_path):
-    preds = set()
-    first = True
-    count = 0
-    with open(mus_file_path, "r") as mus_file:
-        for mus in mus_file:
-            count = count+1
-            if mus.startswith('['):
-                policy = mus.strip()
-                continue
-            preds_set = set(mus.strip('\n,').split(','))
-            if (first):
-                preds = preds.union(preds_set)
-                first = False
-            else:
-                preds = preds.intersection(preds_set)
-    return policy, preds, count-1
-
-def process_mus_intersect_num(mus_file_path, num):
+def process_mus_intersect(mus_file_path, num=10**6):
     preds = set()
     first = True
     count = 0
